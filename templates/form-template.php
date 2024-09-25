52e73e82daf1de3dca146ce7cf14c545765615fb
Umar Khtab <umarkhtab.te@gmail.com>
2024-09-25 18:21:16 +0500

    add css classes in form for styling

--- templates/form-template.php
+++ templates/form-template.php
@@ -1,4 +1,4 @@
-<form id="user-pdf-form" method="post">
+<form class="uk-form-pdf" id="user-pdf-form" method="post">
     <label for="name"><?php esc_html_e( 'Name:', 'user-pdf-form' ); ?></label>
     <input type="text" name="name" id="name" required>
     <label for="email"><?php esc_html_e( 'Email:', 'user-pdf-form' ); ?></label>
